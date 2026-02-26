6cb372a90fb4b2eba5f48dcaa64c27cd381ae2fe
Chris Williams <chris@chrispwill.com>
2026-02-26 17:36:13 +0900

    Use numtide devshell instead of just

--- templates/rust-simple/flake.nix
+++ templates/rust-simple/flake.nix
@@ -7,6 +7,9 @@
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
 
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+
     crane.url = "github:ipetkov/crane";
   };
 
@@ -15,6 +18,9 @@
       inherit inputs;
 
       prefix = "nix/";
-      nixpkgs.overlays = [inputs.crane.overlays.default];
+      nixpkgs.overlays = [
+        inputs.crane.overlays.default
+        inputs.devshell.overlays.default
+      ];
     };
 }
--- templates/rust-simple/nix/devshell.nix
+++ templates/rust-simple/nix/devshell.nix
@@ -1,31 +1,28 @@
 {
-  flake,
   pkgs,
   inputs,
-  system,
   ...
 }: let
   crane = inputs.crane;
   craneLib = crane.mkLib pkgs;
-  moldDevShell = craneLib.devShell.override {
+  devshell = pkgs.devshell.mkShell {
+    imports = [(pkgs.devshell.importTOML ./devshell.toml)];
+  };
+  moldDevshellDevShell = craneLib.devShell.override {
     # For example, use the mold linker
     mkShell = pkgs.mkShell.override {
       stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
     };
   };
 in
-  moldDevShell {
-    packages = with pkgs; [
-      bacon
-      just
-      taplo
-      # miniserve
-    ];
-
-    shellHook = ''
-      echo "Welcome to ${flake.packages.${system}.default.pname}!\n"
-
-      echo Use "just <recipe>" to run a common flow
-      just --list
-    '';
+  moldDevshellDevShell {
+    inputsFrom = [devshell];
+    packages = with pkgs;
+      [
+        bacon
+        just
+        taplo
+        # miniserve
+      ]
+      ++ pkgs.lib.optionals pkgs.stdenv.isLinux [pkgs.gcc];
   }
