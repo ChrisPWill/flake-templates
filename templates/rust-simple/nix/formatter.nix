6507d044defb9d4bb3f2b47328125dbf35e01f86
Chris Williams <chris@chrispwill.com>
2026-02-26 19:34:20 +0900

    Make formatter check on flake build

--- templates/rust-simple/nix/formatter.nix
+++ templates/rust-simple/nix/formatter.nix
@@ -1,32 +1,65 @@
 {
+  flake,
   pname,
   pkgs,
-}:
-pkgs.writeShellApplication {
-  name = pname;
+}: let
+  formatter = pkgs.writeShellApplication {
+    name = pname;
 
-  runtimeInputs = with pkgs; [
-    deadnix
-    alejandra
-  ];
+    runtimeInputs = with pkgs; [
+      deadnix
+      alejandra
+    ];
 
-  text = ''
-    set -euo pipefail
+    text = ''
+      set -euo pipefail
 
-    # If no arguments are passed, default to formatting the whole project
-    if [[ $# = 0 ]]; then
-      prj_root=$(git rev-parse --show-toplevel 2>/dev/null || echo .)
-      set -- "$prj_root"
-    fi
+      # If no arguments are passed, default to formatting the whole project
+      if [[ $# = 0 ]]; then
+        prj_root=$(git rev-parse --show-toplevel 2>/dev/null || echo .)
+        set -- "$prj_root"
+      fi
 
-    set -x
+      set -x
 
-    deadnix --no-lambda-pattern-names --edit "$@"
+      deadnix --no-lambda-pattern-names --edit "$@"
 
-    alejandra "$@"
-  '';
+      alejandra --quiet "$@"
+    '';
 
-  meta = {
-    description = "format your project";
+    meta = {
+      description = "format your project";
+    };
   };
-}
+  check =
+    pkgs.runCommand "format-check" {
+      nativeBuildInputs = [formatter pkgs.git];
+    }
+    ''
+      export HOME=$NIX_BUILD_TOP/home
+
+      # keep timestamps so that treefmt is able to detect mtime changes
+      cp --no-preserve=mode --preserve=timestamps -r ${flake} source
+      cd source
+      git init --quiet
+      git add .
+      shopt -s globstar
+      ${pname} **/*.nix
+      if ! git diff --exit-code; then
+        echo "-------------------------------"
+        echo "aborting due to above changes ^"
+        exit 1
+      fi
+      touch $out
+    '';
+in
+  formatter
+  // {
+    passthru =
+      formatter.passthru
+      // {
+        tests = {
+          check = check;
+        };
+      };
+  }
