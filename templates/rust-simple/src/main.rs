21528eda376f923456b94d1ed9586c995b403f0a
Chris Williams <chris@chrispwill.com>
2026-02-26 20:20:23 +0900

    Add example test

--- templates/rust-simple/src/main.rs
+++ templates/rust-simple/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)] // Ensures this module is only compiled when running tests
+mod tests {
+    #[test] // Marks this function as a test
+    fn it_works() {
+        assert_eq!(true, true);
+    }
+}
